2cfc12e50beb4c0358492b91bac38f89bfce78f9
alu0100767421 <alu0100767421@ull.edu.es>
2016-10-06 15:11:49 +0000

    Añadiendo cambios en el script

--- bin/gitbook-start-alex-moi-nitesh.js
+++ bin/gitbook-start-alex-moi-nitesh.js
@@ -1,9 +1,24 @@
-var fs = require('fs-extra')
+var fs_extra = require('fs-extra')
+var fs = require("fs")
 var path = require("path");
 
 console.log(path.resolve(__dirname))
 
-fs.copy('./template', path.resolve(__dirname), function (err) {
+
+//modificar package.json añadiendo la url del repo del usuario
+
+
+
+//quitar las extensiones ejs a package.json
+fs.rename('../template/package.json.ejs', '../template/package.json', (err) => {
+  if (err) throw err;
+  console.log('renamed complete');
+});
+
+
+//copiar directorio template en el directorio del usuario
+/*
+fs_extra.copy('./template', path.resolve(__dirname, 'template'), function (err) {
   if (err) return console.error(err)
   console.log("success!")
-});
+});*/
