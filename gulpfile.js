bfd5d4fc2d21982568d5ef8ee291a3e131417863
Nitesh Gul Ramnani <alu0100814651@ull.edu.es>
2016-10-19 16:53:14 +0000

    modificaciones

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp  = require('gulp');
 var shell = require('gulp-shell');
+var git = require('gulp-git');
 
+
+gulp.task('buildeploy', ['build', 'deploy']);
 
 gulp.task('build', function() {
   return gulp.src('').pipe(shell(['./scripts/generate-gitbook']));
@@ -11,11 +14,11 @@ gulp.task('deploy', function () {
 });
 
 
-
-/*gulp.task('prueba',function() {
-   return gulp.src('').pipe(shell(['gitbook build `pwd` ./gh-pages'])); 
+gulp.task('deploy-heroku', function () {
+  git.push('heroku', 'master', function (err) {
+    if (err) throw err;
+  });
 });
-*/
 
 
 gulp.task('wikibuild', function() {
@@ -25,3 +28,31 @@ gulp.task('wikibuild', function() {
 gulp.task('wikideploy', function() {
    return gulp.src('').pipe(shell(['./scripts/deploy-wiki'])); 
 });
+
+
+
+
+//deploy de iaas
+
+
+var fs = require('fs');
+var gulp = require('gulp')
+var GulpSSH = require('gulp-ssh')
+ 
+var config = {
+  host: '10.6.128.129',
+  port: 22,
+  username: 'usuario',
+  privateKey: fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)
+}
+ 
+var gulpSSH = new GulpSSH({
+  ignoreErrors: false,
+  sshConfig: config
+})
+
+
+gulp.task('deploy-iaas', function () {
+  return gulpSSH
+    .shell(['cd /home/usuario/src/sytw/iaas', 'git pull']);
+})
